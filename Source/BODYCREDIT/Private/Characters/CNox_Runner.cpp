37e158f55e650440ebf3428edb658fde7282f03d
Likepint <jsuuu96@naver.com>
2025-05-01 17:19:37 +0900

    2025. 05. 01
    캐릭터 이동(걷기, 달리기, 앉기, 점프), 마우스 회전

--- Source/BODYCREDIT/Private/Characters/CNox_Runner.cpp
+++ Source/BODYCREDIT/Private/Characters/CNox_Runner.cpp
@@ -1,9 +1,17 @@
 ﻿#include "Characters/CNox_Runner.h"
 #include "Global.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "InputMappingContext.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "Components/CMovementComponent.h"
 
 ACNox_Runner::ACNox_Runner()
 {
 	PrimaryActorTick.bCanEverTick = true;
+
+	Init();
 
 }
 
@@ -19,8 +27,52 @@ void ACNox_Runner::Tick(float DeltaTime)
 
 }
 
+void ACNox_Runner::NotifyControllerChanged()
+{
+	Super::NotifyControllerChanged();
+
+	// MappingContext
+	if (APlayerController* PlayerController = Cast<APlayerController>(Controller))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+			Subsystem->AddMappingContext(MappingContext, 0);
+	}
+
+}
+
 void ACNox_Runner::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* input = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		// Movement
+		Movement->BindInput(input);
+	}
+
+}
+
+void ACNox_Runner::Init()
+{
+	// Mesh
+	GetMesh()->SetRelativeLocation(FVector(0, 0, ~89));
+	GetMesh()->SetRelativeRotation(FRotator(0, ~89, 0));
+
+	// SpringArm
+	CHelpers::CreateComponent<USpringArmComponent>(this, &SpringArm, "SpringArm", RootComponent);
+	SpringArm->bDoCollisionTest = false;
+	SpringArm->bEnableCameraLag = true;
+	SpringArm->TargetArmLength = 200;
+	SpringArm->bUsePawnControlRotation = true;
+
+	// Camera
+	CHelpers::CreateComponent<UCameraComponent>(this, &Camera, "Camera", SpringArm);
+	Camera->SetRelativeLocation(FVector(~79, 30, 100));
+
+	// MappingContext
+	CHelpers::GetAsset<UInputMappingContext>(&MappingContext, TEXT("/Script/EnhancedInput.InputMappingContext'/Game/Inputs/IMC_Runner.IMC_Runner'"));
+
+	// Movement
+	CHelpers::CreateActorComponent<UCMovementComponent>(this, &Movement, "Movement");
 
 }
---
+++ Source/BODYCREDIT/Public/Interfaces/IBindInput.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "IBindInput.generated.h"
+
+UINTERFACE(MinimalAPI)
+class UIBindInput : public UInterface
+{
+	GENERATED_BODY()
+
+};
+
+class BODYCREDIT_API IIBindInput
+{
+	GENERATED_BODY()
+
+public:
+	virtual void BindInput(class UEnhancedInputComponent* InEnhancedInputComponent) = 0;
+
+};
