9372f989820eb5ef0175bd7aead75e0d6b07bb1d
LHcom <55868914+LHcom@users.noreply.github.com>
2025-05-01 16:22:48 +0900

    Enemy 클래스 생성

---
+++ Source/BODYCREDIT/Public/Characters/Enemy/CNox_EBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Characters/CNox.h"
+#include "CNox_EBase.generated.h"
+
+/**
+ * Enemy Base
+ */
+UCLASS()
+class BODYCREDIT_API ACNox_EBase : public ACNox
+{
+	GENERATED_BODY()
+	
+};
