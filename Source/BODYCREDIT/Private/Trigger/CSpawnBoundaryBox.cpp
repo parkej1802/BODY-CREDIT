3fbde5fd3493d7aae507333c5994bbc2581be713
LHcom <55868914+LHcom@users.noreply.github.com>
2025-06-15 13:13:52 +0900

    스폰박스 별 마리수 지정 스폰

--- Source/BODYCREDIT/Private/Trigger/CSpawnBoundaryBox.cpp
+++ Source/BODYCREDIT/Private/Trigger/CSpawnBoundaryBox.cpp
@@ -28,3 +28,40 @@ FVector ACSpawnBoundaryBox::GetScale() const
 {
 	return BoxComp->GetScaledBoxExtent();
 }
+
+void ACSpawnBoundaryBox::AppendZeroCount()
+{
+	++SpawnCount.CurZeroSpawnCount;
+}
+
+void ACSpawnBoundaryBox::AppendMedicCount()
+{
+	++SpawnCount.CurMedicSpawnCount;
+}
+
+void ACSpawnBoundaryBox::AppendMemoryCount()
+{
+	++SpawnCount.CurMemorySpawnCount;
+}
+
+void ACSpawnBoundaryBox::ResetCount()
+{
+	SpawnCount.CurZeroSpawnCount = 0;
+	SpawnCount.CurMedicSpawnCount = 0;
+	SpawnCount.CurMemorySpawnCount = 0;
+}
+
+bool ACSpawnBoundaryBox::CanSpawnZero()
+{
+	return SpawnCount.CurZeroSpawnCount < SpawnCount.ZeroSpawnCount;
+}
+
+bool ACSpawnBoundaryBox::CanSpawnMedic()
+{
+	return SpawnCount.CurMedicSpawnCount < SpawnCount.MedicSpawnCount;
+}
+
+bool ACSpawnBoundaryBox::CanSpawnMemory()
+{
+	return SpawnCount.CurMemorySpawnCount < SpawnCount.MemorySpawnCount;
+}
